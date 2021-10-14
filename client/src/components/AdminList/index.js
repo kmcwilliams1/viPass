a6212bc225fb688b33b1f9bec3501b21d8ff1f29
Kyle McWilliams <kmac52694@gmail.com>
2021-10-14 09:55:31 -0500

    adding tier model & ___List.js components

--- client/src/components/AdminList/index.js
+++ client/src/components/AdminList/index.js
@@ -1,42 +1,18 @@
-import React, {useState} from 'react';
+// import React, {useState} from 'react';
 
 
 
-const AdminList =({
-  // isAdmin = true,
-}) => {
-  const [admins] = useState(['email@email.com', 'email2@email.com']);
-  if (!admins.length) {
-    return <h3>No Admins Yet</h3>;
-  }
-
-  return (
-    <section >
-        <button onClick = "makeAdmin()" >Add Admin</button> 
-      <div id="container" className="container">
-        {admins.map((admin) => (
-          <div className="box">
-            {admin}
-          </div>
-        ))}
-      </div>
-    </section>
-  );
-}
-
-export default AdminList
-
-
-// import React from "react";
-// import ".../index.css"
-
-
-
-// function Admin() {
+// const AdminList =({
+//   // isAdmin = true,
+// }) => {
 //   const [admins] = useState(['email@email.com', 'email2@email.com']);
+//   if (!admins.length) {
+//     return <h3>No Admins Yet</h3>;
+//   }
+
 //   return (
 //     <section >
-//         <button onClick = "addAdmin()" >Add Admin</button> 
+//         <button onClick = "makeAdmin()" >Add Admin</button> 
 //       <div id="container" className="container">
 //         {admins.map((admin) => (
 //           <div className="box">
@@ -48,4 +24,81 @@ export default AdminList
 //   );
 // }
 
-// export default Admin
+// export default AdminList
+
+
+import React , {useState}  from "react";
+import {
+  Jumbotron,
+  Container,
+  CardColumns,
+  Card,
+  Button,
+} from "react-bootstrap";
+import { useQuery, useMutation } from "@apollo/client";
+import Auth from "../utils/auth";
+import { REMOVE_ADMIN } from "../utils/mutations";
+import { QUERY_ADMINS } from "../utils/queries";
+const AdminList = () => {
+  const [removeAdmin] = useMutation(REMOVE_ADMIN);
+  const { loading, data } = useQuery(QUERY_ADMINS);
+  const userData = data?.me || {};
+  
+  // create function that accepts the book's mongo _id value as param and deletes the book from the database
+  const handleDeleteAdmin = async (username) => {
+    const token = Auth.loggedIn() ? Auth.getToken() : null;
+    if (!token) {
+      return false;
+    }
+    try {
+      const { data } = await removeAdmin({
+        variables: { username },
+      });
+      console.log(data);
+      return data;
+    } catch (err) {
+      console.error(err);
+    }
+  };
+  // if data isn't here yet, say so
+  if (loading) {
+    return <h2>LOADING...</h2>;
+  }
+  return (
+    <>
+      <Jumbotron fluid className="text-light bg-dark">
+        <Container>
+          <h1>Viewing admins</h1>
+        </Container>
+      </Jumbotron>
+      <Container>
+        <h2>
+          {userData.username?.length
+            ? `Viewing ${userData.username.length} saved ${
+                userData.username.length === 1 ? "user" : "users"
+              }:`
+            : "You have no amdins!"}
+        </h2>
+        <CardColumns>
+          {userData.username?.map((users) => {
+            return (
+              <Card key={users.username} border="dark">
+                <Card.Body>
+                  <Card.Title>{users.email}</Card.Title>
+                  <Card.Text>{users.permissions}</Card.Text>
+                  <Button
+                    className="btn-block btn-danger"
+                    onClick={() => handleDeleteAdmin(users.username)}
+                  >
+                    Delete this user!
+                  </Button>
+                </Card.Body>
+              </Card>
+            );
+          })}
+        </CardColumns>
+      </Container>
+    </>
+  );
+};
+export default AdminList;
