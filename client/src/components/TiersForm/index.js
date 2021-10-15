342d1bc297b11a760d5ad8609e516e37d649e65b
Kyle McWilliams <kmac52694@gmail.com>
2021-10-15 11:39:39 -0500

    working front end, now for the routes

--- client/src/components/TiersForm/index.js
+++ client/src/components/TiersForm/index.js
@@ -1,106 +1,97 @@
-// import React, { useState } from 'react';
-// import { Link } from 'react-router-dom';
-// import { useMutation } from '@apollo/client';
+import React, { useState } from 'react';
+import { Link } from 'react-router-dom';
+import { useMutation } from '@apollo/client';
+import { ADD_TIER } from '../../utils/mutations';
+import { QUERY_TIERS} from '../../utils/queries';
+import Auth from '../../utils/auth';
 
-// import { ADD_TIER } from '../../utils/mutations';
-// import { QUERY_TIERS} from '../../utils/queries';
+const TiersForm = () => {
+  const [newTierText, setNewTierText] = useState('');
+  const [addTier, { error }] = useMutation(ADD_TIER, {
+    update(cache, { data: { addTier } }) {
+      try {
+        const { tiers } = cache.readQuery({ query: QUERY_TIERS });
+            //is it QUERY_TIERS?  
+        cache.writeQuery({
+          query: QUERY_TIERS,
+          data: { tiers: [addTier, ...tiers] },
+          //is it ...users??
+        });
+      } catch (e) {
+        console.error(e);
+      }
+    },
+  });
+  const handleFormSubmit = async (event) => {
+    event.preventDefault();
 
-// import Auth from '../../utils/auth';
-
-// const TiersForm = () => {
-//   const [newTierText, setNewTierText] = useState('');
-
-
-//   const [addTier, { error }] = useMutation(ADD_TIER, {
-//     update(cache, { data: { addTier } }) {
-//       try {
-//         const { tiers } = cache.readQuery({ query: QUERY_TIERS });
-//             //is it QUERY_TIERS?  
-//         cache.writeQuery({
-//           query: QUERY_TIERS,
-//           data: { tiers: [addTier, ...tiers] },
-//           //is it ...users??
-//         });
-//       } catch (e) {
-//         console.error(e);
-//       }
-//     },
-//   });
-
-//   const handleFormSubmit = async (event) => {
-//     event.preventDefault();
-
-//     try {
-//       const { data } = await addTier({
-//         variables: {
-//           newTierText,
-//         },
-//       });
+    try {
+      const { data } = await addTier({
+        variables: {
+          newTierText,
+        },
+      });
       
-//       console.log(data)
-//       setNewTierText('');
-//     } catch (err) {
-//       console.error(err);
-//     }
-//   };
+      console.log(data)
+      setNewTierText('');
+    } catch (err) {
+      console.error(err);
+    }
+  };
+  const handleChange = (event) => {
+    const { name, value } = event.target;
 
-//   const handleChange = (event) => {
-//     const { name, value } = event.target;
+    if (name === 'newTierText') {
+      setNewTierText(value);
+    }
+  };
 
-//     if (name === 'newTierText') {
-//       setNewTierText(value);
-//     }
-//   };
+  return (
+    <div>
+      {Auth.loggedIn() ? (
+        <>
+          <p
+            className={`m-0 ${
+              characterCount === 280 || error ? 'text-danger' : ''
+            }`}
+          >
+            Character Count: {characterCount}/280
+          </p>
+          <form
+            className="flex-row justify-center justify-space-between-md align-center"
+            onSubmit={handleFormSubmit}
+          >
+            <div className="col-12 col-lg-9">
+              <textarea
+                name="newTierText"
+                placeholder="What is the new tier?"
+                value={newTierText}
+                className="form-input w-100"
+                style={{ lineHeight: '1.5', resize: 'vertical' }}
+                onChange={handleChange}
+              ></textarea>
+            </div>
 
+            <div className="col-12 col-lg-3">
+              <button className="btn btn-primary btn-block py-3" type="submit">
+                Add Tier
+              </button>
+            </div>
+            {error && (
+              <div className="col-12 my-3 bg-danger text-white p-3">
+                {error.message}
+              </div>
+            )}
+          </form>
+        </>
+      ) : (
+        <p>
+          You need to be an admin to create new tiers. Please{' '}
+          <Link to="/login">login</Link> or <Link to="/signup">signup.</Link>
+        </p>
+      )}
+    </div>
+  );
+};
 
-
-
-//   return (
-//     <div>
-//       {Auth.loggedIn() ? (
-//         <>
-//           <p
-//             className={`m-0 ${
-//               characterCount === 280 || error ? 'text-danger' : ''
-//             }`}
-//           >
-//             Character Count: {characterCount}/280
-//           </p>
-//           <form
-//             className="flex-row justify-center justify-space-between-md align-center"
-//             onSubmit={handleFormSubmit}
-//           >
-//             <div className="col-12 col-lg-9">
-//               <textarea
-//                 name="newTierText"
-//                 placeholder="What is the new permission?"
-//                 value={newTierText}
-//                 className="form-input w-100"
-//                 style={{ lineHeight: '1.5', resize: 'vertical' }}
-//                 onChange={handleChange}
-//               ></textarea>
-//             </div>
-
-//             <div className="col-12 col-lg-3">
-//               <button className="btn btn-primary btn-block py-3" type="submit">
-//                 Add Tier
-//               </button>
-//             </div>
-//             {error && (
-//               <div className="col-12 my-3 bg-danger text-white p-3">
-//                 {error.message}
-//               </div>
-//             )}
-//           </form>
-//         </>
-//       ) : (
-//         <p>
-//           You need to be an admin to create new tiers. Please{' '}
-//           <Link to="/login">login</Link> or <Link to="/signup">signup.</Link>
-//         </p>
-//       )}
-//     </div>
-//   );
-// };
-
-// export default TiersForm;
+export default TiersForm;
--- client/src/pages/Home.js
+++ client/src/pages/Home.js
@@ -1,18 +1,19 @@
 import React from 'react';
-import { useQuery } from '@apollo/client';
+// import { useQuery } from '@apollo/client';
 
 import { Link } from 'react-router-dom';
 
 // import Permissions from './Permissions';
 
-import { QUERY_PERMISSIONS } from '../utils/queries';
-import Admins from './Admins';
+// import { QUERY_PERMISSIONS } from '../utils/queries';
+// import Admins from './Admins';
 
 
 const Home = () => {
-  const { data } = useQuery(QUERY_PERMISSIONS);
+  // const { data } = useQuery(QUERY_PERMISSIONS);
   // const permissions = data?.permissions || [];
-  const user = data?.user || [];
+  // const admins = data?.admins || [];
+  // const tier = data?.tier || [];
 
   return (
     <main>
@@ -27,14 +28,15 @@ const Home = () => {
           <Link className="button is-link is-focused" to="/signup">
             Signup
           </Link>
-          <ClientHome/>
+          
+          {/* <ClientHome/>
           <Permissions  permissions={permissions} />
-          <AdminList>
-            // admins={admins}
-          {/* <Permissions  permissions={permissions} /> */}
+          <AdminList admins={admins} />
+            
+           <Permissions  permissions={permissions} /> 
           <Admins
             users={user}
-          />
+          /> */}
         </div>
       </div>
     </main>
